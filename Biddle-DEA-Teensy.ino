286c76c3a3ea07d83ef12705b200f521685d4b24
Kyle Thompson <kt49@buffalo.edu>
2015-07-31 12:35:10 -0400

    Working Well
    Increased voltage range, lower noise, separated PD and Voltage serial
    packets

--- Biddle-DEA-Teensy.ino
+++ Biddle-DEA-Teensy.ino
@@ -3,9 +3,7 @@
 
 //TODO
 //Test
-//Uncomment Serialcomm
-//Uncomment AC lines
-
+//Divide by 10
 #include <ADC.h>
 
 ADC *adc = new ADC(); // adc object
@@ -24,7 +22,8 @@ void setup() {
   Serial1.begin(115200);
   pinMode(A10, INPUT); //Diff Channel 0 Positive
   pinMode(A11, INPUT); //Diff Channel 0 Negative
-
+  pinMode(A12, INPUT); //Diff Channel 1 Positive
+  pinMode(A13, INPUT); //Diff Channel 1 Negative
   /********DC VOLTAGE (ADC CHANNEL 0) ***************/
   adc->setReference(ADC_REF_1V2, ADC_0);
   adc->setAveraging(32); // set number of averages
@@ -37,7 +36,7 @@ void setup() {
   adc->setConversionSpeed(ADC_LOW_SPEED); // change the conversion speed
   // it can be ADC_VERY_LOW_SPEED, ADC_LOW_SPEED, ADC_MED_SPEED, ADC_HIGH_SPEED or ADC_VERY_HIGH_SPEED
   adc->setSamplingSpeed(ADC_LOW_SPEED); // change the sampling speed
-  adc->startContinuousDifferential(A10, A11, ADC_0);
+  //  adc->startContinuousDifferential(A10, A11, ADC_0);
 
   /********AC VOLTAGE (ADC CHANNEL 1) ***************/
   adc->setReference(ADC_REF_1V2, ADC_1);
@@ -52,26 +51,27 @@ void setup() {
   // it can be ADC_VERY_LOW_SPEED, ADC_LOW_SPEED, ADC_MED_SPEED, ADC_HIGH_SPEED or ADC_VERY_HIGH_SPEED
   adc->setSamplingSpeed(ADC_LOW_SPEED, ADC_1); // change the sampling speed
   // always call the compare functions after changing the resolution!
-  adc->enableCompare(1.2 / 1.2 * adc->getMaxValue(ADC_1), 0, ADC_1); // measurement will be ready if value < 1.0V
-  adc->startContinuousDifferential(A12, A13, ADC_1);
+  //  adc->enableCompare(1.2 / 1.2 * adc->getMaxValue(ADC_1), 0, ADC_1); // measurement will be ready if value < 1.0V
+  //  adc->startContinuousDifferential(A12, A13, ADC_1);
 
-  delay(2000);
+  delay(1000);
 
-  DCVoltage = (float)adc->analogReadContinuous(ADC_0) / (4096.0) * 1.2 / DCpga;
-  ACVoltage = (float)adc->analogReadContinuous(ADC_1) / (4096.0) * 1.2 / ACpga;
-  Serial.write('%');
-  printVoltage(DCVoltage);
-  Serial.write('~');
-  printVoltage(ACVoltage);
+  //  DCVoltage = (float)adc->analogReadDifferential(A10,A11,ADC_0) / (4096.0) * 1.2 / DCpga;
+  //  ACVoltage = -(float)adc->analogReadDifferential(A12,A13,ADC_1) / (4096.0) * 1.2 / ACpga;//Wired Backwords
+  //  Serial.write('%');
+  //  printVoltage(DCVoltage);
+  //  Serial.write('~');
+  //  printVoltage(ACVoltage);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  //serialComm();
+  //  serialComm();
+  delay(5);
   voltageReadWrite();
 
   //5 milliseconds is needed to seperate serial packets
-  delay(5);
+  //  delay(5);
 
 }
 
@@ -107,10 +107,10 @@ void serialComm() {
 
 void printVoltage(float voltage) {
   if (voltage < 10) {
-    Serial.println(voltage, 2);
+    Serial.println(voltage, 3);
   }
   else {
-    Serial.println(voltage, 1);
+    Serial.println(voltage, 2);
   }
 }
 
@@ -119,26 +119,64 @@ void voltageReadWrite() {
   float changeThres = 0.001;
   static float prevDC;
   static float prevAC;
-  DCVoltage = (float)adc->analogReadContinuous(ADC_0) / (4096.0) * 1.2 / DCpga;
-  ACVoltage = (float)adc->analogReadContinuous(ADC_1) / (4096.0) * 1.2 / ACpga;
-  if (abs(prevDC - DCVoltage) > changeThres) {
-    Serial.write('%');
-    printVoltage(DCVoltage / 10);
-  }
-  if (ACVoltage >= 0.25 && ACpga == 4) {
-    ACpga = 2;
-    adc->enablePGA(ACpga);
-    adc->startContinuousDifferential(A10, A11, ADC_0);
-  }
-  if (ACVoltage < 0.2 && ACpga == 2) {
-    ACpga = 4;
-    adc->enablePGA(ACpga);
-    adc->startContinuousDifferential(A10, A11, ADC_0);
-  }
-  //  if (abs(prevAC - ACVoltage) > changeThres) {
-  //    Serial.write('~');
-  //    printVoltage(ACVoltage/10);
+  DCVoltage = (float)adc->analogReadDifferential(A10, A11, ADC_0) / (4096.0) * 1.2 / DCpga;
+  ACVoltage = -(float)adc->analogReadDifferential(A12, A13, ADC_1) / (4096.0) * 1.2 / ACpga; //Wired Backwards
+  //  if (abs(prevDC - DCVoltage) > changeThres) {
+  //    Serial.write('%');
+  //    printVoltage(DCVoltage);
   //  }
+  ACgainChange(ACVoltage);
+  DCgainChange(DCVoltage);
+  if (abs(prevAC - ACVoltage) > changeThres) {
+    Serial.write('~');
+    printVoltage(ACVoltage);
+  }
   prevDC = DCVoltage;
   prevAC = ACVoltage;
 }
+
+void ACgainChange(float voltage) {
+  if (voltage < 0.01875) {
+    ACpga = 64;
+  }
+  else if (voltage < 0.0375) {
+    ACpga = 32;
+  }
+  else if (voltage < 0.075) {
+    ACpga = 16;
+  }
+  else if (voltage < 0.15) {
+    ACpga = 8;
+  }
+  else if (voltage < 0.3) {
+    ACpga = 4;
+  }
+  else {
+    ACpga = 2;
+  }
+  adc->enablePGA(ACpga, ADC_1);
+}
+
+void DCgainChange(float voltage) {
+  if (voltage < 0.01875) {
+    DCpga = 64;
+  }
+  else if (voltage < 0.0375) {
+    DCpga = 32;
+  }
+  else if (voltage < 0.075) {
+    DCpga = 16;
+  }
+  else if (voltage < 0.15) {
+    DCpga = 8;
+  }
+  else if (voltage < 0.3) {
+    DCpga = 4;
+  }
+  else {
+    DCpga = 2;
+  }
+  adc->enablePGA(DCpga);
+}
+
+
